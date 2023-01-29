a1696d56c4b16033460c7dc727af4b25869a83c6
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-01-29 21:16:03 +0900

    Dollar, Franc 클래스 제거

--- tdd-study/src/main/java/Money.java
+++ tdd-study/src/main/java/Money.java
@@ -1,18 +1,32 @@
 
-abstract class Money {
-    abstract Money times(int multiplier);
+class Money {
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+    String currency() {
+        return currency;
+    }
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Money(amount, "USD");
     }
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Money(amount, "CHF");
     }
 
     protected int amount;
-
+    protected String currency;
     @Override
     public boolean equals(Object o) {
         Money money = (Money) o;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
     }
 }
--- tdd-study/src/test/java/DollarTest.java
+++ tdd-study/src/test/java/DollarTest.java
@@ -11,12 +11,6 @@ class DollarTest {
         assertEquals(Money.dollar(15), five.times(3));
     }
     @Test
-    void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-    @Test
     void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
@@ -24,4 +18,5 @@ class DollarTest {
         assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
+
 }
