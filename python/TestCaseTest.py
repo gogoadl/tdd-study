80946c0050cff0dd3fa8237b07b62d2cd7bfef47
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-02-12 17:06:41 +0900

    20장 내용 누락분 적용

--- python/TestCaseTest.py
+++ python/TestCaseTest.py
@@ -6,13 +6,17 @@ class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
 
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown" == test.log)
     def testRunning(self):
         self.test.run()
         assert (self.test.wasRun)
 
     def testSetUp(self):
         self.test.run()
-        assert ("setUp" == self.test.log)
+        assert ("setUp testMethod" == self.test.log)
 
 
 TestCaseTest("testRunning").run()
