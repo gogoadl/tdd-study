62cedfef50878c4a3e4b6addd9dfc1993b169373
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-02-12 22:10:57 +0900

    22장 내용 적용

--- python/TestResult.py
+++ python/TestResult.py
@@ -1,9 +1,13 @@
 class TestResult:
     def __init__(self):
-        self.runCount = 1
+        self.runCount = 0
+        self.failedCount = 0
 
     def testStarted(self):
         self.runCount = self.runCount + 1
 
+    def testFailed(self):
+        self.failedCount = self.failedCount + 1
+
     def summary(self):
-        return "%d run, 0 failed" % self.runCount
+        return "%d run, %d failed" % (self.runCount, self.failedCount)
